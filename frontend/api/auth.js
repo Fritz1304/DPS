77e8d3f6ea30afe385cbb49ae339acad9ab84474
JuanCVargasC <102472980+JuanCVargasC@users.noreply.github.com>
2024-03-10 18:26:23 -0500

    Request update

--- frontend/api/auth.js
+++ frontend/api/auth.js
@@ -1,5 +1,5 @@
 import axios from "./axios";
-
+// request User,register,abogado, login
 const API = 'https://dps-sfyn.onrender.com/api'
 export const registerRequest= user => axios.post(`/register`,user) 
 export const registerAbogaRequest= abogado => axios.post(`/abogadoregister`,abogado)     
